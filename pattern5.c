deadb3219aed6fa60aa723371952300733a1e222
Garvit Jain <Garvit.26685@stu.upes.ac.in>
2025-09-17 01:05:51 +0530

    Add files via upload

---
+++ pattern5.c
@@ -0,0 +1,20 @@
+/* program to print the following pattern:
+    *****
+    ****
+    ***
+    **
+    *       */
+
+#include<stdio.h>
+int main () {
+   for (int i=5;i>=1;i--) {
+        for(int j=1;j<=i;j++){
+            if (j==i){
+                printf("*\n");
+            } else {
+                printf("*");
+            }
+        }
+    }
+    return 0;
+}
