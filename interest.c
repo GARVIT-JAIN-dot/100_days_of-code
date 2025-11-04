ee5f82fcb93e04e300247c0a1db53fde2989a56b
Garvit Jain <Garvit.26685@stu.upes.ac.in>
2025-11-05 01:06:52 +0530

    q_9

--- interest.c
+++ interest.c
@@ -1,4 +1,18 @@
-//program to calculate simple and compound interest for given principal, rate, and time.
+Q9: Write a program to calculate simple and compound interest for given principal, rate, and time.
+
+/*
+Sample Test Cases:
+Input 1:
+1000 5 2
+Output 1:
+Simple Interest=100, Compound Interest=102.5
+
+Input 2:
+5000 7 3
+Output 2:
+Simple Interest=1050, Compound Interest=1125.76
+
+*/
    
 #include<stdio.h>
 #include<math.h>
@@ -19,3 +33,4 @@ int main(){
     return 0;
 
 }
+
