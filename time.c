62206095c8085ec69016ffe4c36e22aa35b494da
Garvit Jain <Garvit.26685@stu.upes.ac.in>
2025-11-05 01:07:40 +0530

    q_10

--- time.c
+++ time.c
@@ -1,4 +1,18 @@
-// program to input time in seconds and convert it to hours:minutes:seconds format.
+Q10: Write a program to input time in seconds and convert it to hours:minutes:seconds format.
+
+/*
+Sample Test Cases:
+Input 1:
+3661
+Output 1:
+1:1:1
+
+Input 2:
+7322
+Output 2:
+2:2:2
+
+*/
   
 #include <stdio.h>
 int main (){
@@ -14,3 +28,4 @@ int main (){
     return 0;
 
 }
+
